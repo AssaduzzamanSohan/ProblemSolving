7b4534542e93bd71135d1cd1c274231e0a0f3b7f
Kh. Assaduzzaman Sohan <46751491+AssaduzzamanSohan@users.noreply.github.com>
2022-09-04 16:00:58 +0600

    Create I. Hovercraft.java

---
+++ 2021 ICPC Asia Dhaka Regional Online Preliminary/I. Hovercraft.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        FastScanner fs = new FastScanner();
+
+        while (true) {
+            int l = fs.nextInt();
+            int w = fs.nextInt();
+
+            if (l == 0 && w == 0) break;
+
+            double ans = Double.MAX_VALUE;
+
+            for (double i = (double) l / 2; i < l; i += 0.01) {
+                double base = l - i;
+                double height = Math.sqrt((i * i) - (base * base));
+                ans = Math.min(ans, (w - height) + i + i);
+            }
+            System.out.printf("%.4f\n", ans);
+        }
+    }
+
+    static class FastScanner {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer("");
+
+        String next() {
+            while (!st.hasMoreTokens())
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        int[] readArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+    }
+}
